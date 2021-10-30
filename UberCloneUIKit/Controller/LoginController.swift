6edc35cd8a637d5d357e622146157f912bef68c8
Caleb Danielsen <caleb@lfpost.dk>
2021-10-30 22:32:39 +0200

    Project ready for programming without storyboard. Now the UIViewController starts the the SceneDelegate

--- UberCloneUIKit/Controller/LoginController.swift
+++ UberCloneUIKit/Controller/LoginController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  UberCloneUIKit
 //
 //  Created by Caleb Danielsen on 30/10/2021.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
