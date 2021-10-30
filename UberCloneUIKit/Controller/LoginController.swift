0d1e90dae74a202d314848f577de76ddda91be95
Caleb Danielsen <caleb@lfpost.dk>
2021-10-30 23:39:42 +0200

    small change

--- UberCloneUIKit/Controller/LoginController.swift
+++ UberCloneUIKit/Controller/LoginController.swift
@@ -9,8 +9,44 @@ import UIKit
 
 class LoginController: UIViewController {
 
+    // MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    private let emailContainerView: UIView = {
+        let view = UIView()
+        
+        let imageView = UIImageView()
+        
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.attributedPlaceholder = NSAttributedString(string: "", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        return tf
+    }()
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
     }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
 }
