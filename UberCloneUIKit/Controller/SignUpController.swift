d9f9610eb80bfb1b6277557b81dc39d7760ce981
Caleb Danielsen <caleb@lfpost.dk>
2021-11-11 23:27:11 +0100

    Login UI complete

---
+++ UberCloneUIKit/Controller/SignUpController.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpController.swift
+//  UberCloneUIKit
+//
+//  Created by Caleb Danielsen on 11/11/2021.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Selectors
+}
